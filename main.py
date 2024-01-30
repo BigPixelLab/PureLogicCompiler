37e4dc789845e03e615b4db41d8671d154be5903
BigPixelLab1 <iam-namber-1@mail.ru>
2024-01-30 23:38:28 +0300

    Improved application structure

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import argparse
 import datetime
 import os
-import subprocess
-import sys
+from itertools import chain
+from typing import Optional, Iterable
 
 from classes.database_schema_builder import DatabaseSchemaBuilder
 from classes.sql_builder import PostgreSqlBuilder, DEFAULT_TYPE_TABLE
+from classes.types import DbSchema
 
 
-class CheckExecutionTime:
+class ExecutionTimeContextManager:
+    """ Класс позволяющий замерить время выполнения блока кода,
+       находящегося в контекстном менеджере """
+
     def __init__(self):
         self._start_time = None
         self._end_time = None
@@ -25,61 +29,45 @@ class CheckExecutionTime:
         return self._total_time.total_seconds() * 10**3
 
 
-def setup(args):
+class Application:
+    def __init__(self):
+        self._args: Optional[argparse.Namespace] = None
 
-    timer = CheckExecutionTime()
+    def notify(self, *args, sep: str = ' ', end: str = '\n'):
+        """ Метод для вывода оповещений пользователю """
 
-    with timer:
-        print('Обновляем pip...')
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
+        if getattr(self._args, 'silent', False):
+            return
+        print(*args, sep=sep, end=end)
 
-        print('\n\nУстановка PyYaml...')
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'PyYaml'])
+    def output(self, *args, sep: str = ' ', end: str = '\n', create_file: bool = False):
+        """ Метод для вывода результата работы программы """
 
-        print('\n\nУстановка pydantic...')
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pydantic'])
+        filename = getattr(self._args, 'output', None)
+        encoding = getattr(self._args, 'encoding', 'utf-8')
+        mode = ['at', 'xt'][create_file]
 
-    print(f'\n\nУстановка завершена за {timer.total():.03f}мс.\n')
+        if filename is None and create_file:
+            os.system('cls')
 
+        if filename is None:
+            print(*args, sep=sep, end=end)
+            return
 
-def compile_to_ddl(args):
+        with open(filename, mode, encoding=encoding) as file:
+            print(*args, sep=sep, end=end, file=file)
 
-    if not os.path.exists(args.input):
-        print('Указанный в качестве источника схемы путь не существует.')
+    def error(self, exception: Exception):
+        """ Метод для вывода сообщений об ошибке """
 
-    if os.path.isdir(args.input) and args.recursive:
-        schema_files = []
+        print('ERROR:', *exception.args)
 
-        for directory, _, files in os.walk(args.input):
-            schema_files.extend(
-                os.path.join(directory, file)
-                for file in files
-            )
+    def _load_schema(self, schema_files: Iterable[str]) -> Optional[DbSchema]:
+        dsb = DatabaseSchemaBuilder()
 
-    elif os.path.isdir(args.input) and not args.recursive:
-        schema_files = [
-            path
-            for path in os.listdir(args.input)
-            if os.path.isfile(path)
-        ]
-
-    elif os.path.isfile(args.input):
-        schema_files = [args.input]
-
-    else:
-        print('Указанный в качестве источника схемы путь не ведёт к файлу или директории')
-        return
-
-    dsb = DatabaseSchemaBuilder()
-    timer = CheckExecutionTime()
-
-    if not args.silent:
-        print('Поехали!')
-
-    with timer:
+        self.notify('ЗАГРУЗКА СХЕМЫ:')
         for path in schema_files:
-            if not args.silent:
-                print(f'Обработка файла "{path}"...  ', end='')
+            self.notify(f'Обработка файла "{path}"...  ', end='')
 
             with open(path, 'rt', encoding='utf-8') as file:
                 content = file.read()
@@ -87,69 +75,117 @@ def compile_to_ddl(args):
             try:
                 dsb.partial_load(content)
             except ValueError as error:
-                if not args.silent:
-                    print(f'ОШИБКА')
-
-                print('ERROR:', *error.args)
+                self.notify(f'ОШИБКА')
+                self.error(error)
                 return
 
-            if not args.silent:
-                print(f'100%')
+            self.notify(f'100%')
 
         schema = dsb.finalize()
+        self.notify('готово.')
 
-    builder = PostgreSqlBuilder(schema, restrict_types_table=DEFAULT_TYPE_TABLE)
-    ddl = builder.get_db_sql()
+        return schema
 
-    if args.output is not None:
-        with open(args.output, 'wt', encoding='utf-8') as file:
-            file.write(ddl)
-    else:
-        print(ddl)
-        print()
+    def _generate_ddl(self, schema: DbSchema) -> Optional[str]:
+        self.notify('\nГЕНЕРАЦИЯ DDL:')
+        builder = PostgreSqlBuilder(schema, restrict_types_table=DEFAULT_TYPE_TABLE)
 
-    if not args.silent:
-        print(f'Компиляция завершена за {timer.total():.03f}мс.\n')
+        try:
+            ddl = builder.get_db_sql()
+        except ValueError as error:
+            self.error(error)
+            return
+
+        self.notify('готово.')
+
+        return ddl
+
+    def compile_to_ddl(self):
+        try:
+            schema_files = get_files(self._args.input, recursive=self._args.recursive)
+        except Exception as error:
+            print('ERROR:', *error.args)
+            return
+
+        with (timer := ExecutionTimeContextManager()):
+            if (schema := self._load_schema(schema_files)) is None:
+                return
+
+            if (ddl := self._generate_ddl(schema)) is None:
+                return
+
+        self.output(ddl, create_file=True)
+        self.notify(f'Компиляция завершена за {timer.total():.03f}мс.\n')
+
+    def run(self):
+        parser = argparse.ArgumentParser(
+            description='Инструмент для работы со схемами баз данных, '
+                        'описанных в формате PureLogic.'
+        )
+
+        subparsers = parser.add_subparsers(
+            title='подкомманды',
+            description='доступные подкомманды'
+        )
+
+        # Комманды для компиляции
+        compile_parser = subparsers.add_parser('compile',
+                                               help='Конпиляция схемы в DDL')
+        compile_parser.add_argument('input',
+                                    help='Путь к файлу или директории содержащей схему')
+        compile_parser.add_argument('-o', '--output', default=None,
+                                    help='Путь к месту сохранения DDL скрипта. '
+                                         'Если не указан, скрипт будет выведен в stdout')
+        compile_parser.add_argument('--encoding', default=None,
+                                    help='Кодировка входных файлов. Если указан output, '
+                                         'произведённый файл будет в этой же кодировке. '
+                                         'По-умолчанию используется "utf-8"')
+        compile_parser.add_argument('-r', '--recursive', action='store_true',
+                                    help='Рекурсивный обход директории, содержащей схему')
+        compile_parser.add_argument('-s', '--silent', action='store_true',
+                                    help='Не выводить информацию предназначенную для пользователя')
+        compile_parser.set_defaults(function=self.compile_to_ddl)
+
+        self._args: argparse.Namespace = parser.parse_args()
+
+        if hasattr(self._args, 'function'):
+            self._args.function()
 
 
-def parse_cmd_arguments():
-    parser = argparse.ArgumentParser(
-        description='Инструмент для работы со схемами баз данных, '
-                    'описанных в формате PureLogic.'
+def get_files(path: str, recursive: bool = True):
+    """ Возвращает список файлов, лежащих по указанному пути """
+
+    if not os.path.exists(path):
+        raise FileNotFoundError('Указанный в качестве источника схемы путь не существует.')
+
+    if os.path.isfile(path):
+        return [path]
+
+    if not os.path.isdir(path):
+        raise OSError('Указанный в качестве источника схемы путь не ведёт к файлу или директории')
+
+    if not recursive:
+        # Получаем содержимое директории с помощью listdir, клеим к каждому
+        # элементу базовый путь, фильтруем то что файл
+        return list(
+            filter(os.path.isfile, (
+                os.path.join(path, file)
+                for file in os.listdir(path)
+            ))
+        )
+
+    # Просим os.walk рекурсивно выдавать директории и файлы в них, к файлам
+    # клеим путь директории, выпрямляем всё в сплошной список
+    return list(
+        chain(*[
+            [os.path.join(directory, file) for file in files]
+            for (directory, _, files) in os.walk(path)
+        ])
     )
-
-    subparsers = parser.add_subparsers(
-        title='подкомманды',
-        description='доступные подкомманды'
-    )
-
-    # Команда для установки
-    setup_parser = subparsers.add_parser('setup',
-                                         help='Установить необходимые пакеты')
-    setup_parser.set_defaults(function=setup)
-
-    # Комманды для компиляции
-    compile_parser = subparsers.add_parser('compile',
-                                           help='Конпиляция схемы в DDL')
-    compile_parser.add_argument('input',
-                                help='Путь к файлу или директории содержащей схему')
-    compile_parser.add_argument('-o', '--output', default=None,
-                                help='Путь к месту сохранения DDL скрипта. '
-                                     'Если не указан, скрипт будет выведен в stdout')
-    compile_parser.add_argument('-r', '--recursive', action='store_true',
-                                help='Рекурсивный обход директории, содержащей схему')
-    compile_parser.add_argument('-s', '--silent', action='store_true',
-                                help='Не выводить информацию предназначенную для пользователя')
-    compile_parser.set_defaults(function=compile_to_ddl)
-
-    args: argparse.Namespace = parser.parse_args()
-
-    if hasattr(args, 'function'):
-        args.function(args)
 
 
 if __name__ == '__main__':
-    parse_cmd_arguments()
+    Application().run()
 
 
 # table = dsb.tables[1]
